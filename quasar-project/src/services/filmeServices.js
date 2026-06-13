4502b89d49072af74b8e26d728af602993337d76
gfreitas15 <gfmonguilhott793@gmail.com>
2026-06-13 14:23:42 -0300

    feat: create filmes structure

--- quasar-project/src/services/filmeServices.js
+++ quasar-project/src/services/filmeServices.js
@@ -1,11 +1,13 @@
-function getFilmesCountFromRest() {
+import { API_URL } from '@/services/api.js'
+
+function getAllFilmesFromRest() {
   return new Promise((resolve, reject) => {
-    fetch('http://localhost:3000/filmes')
+    fetch(`${API_URL}/filmes`)
       .then((response) => {
         response
           .json()
           .then((data) => {
-            resolve(data.length)
+            resolve(data)
           })
           .catch((error) => {
             reject(error)
@@ -17,4 +19,82 @@ function getFilmesCountFromRest() {
   })
 }
 
-export { getFilmesCountFromRest }
+function getFilmesCountFromRest() {
+  return new Promise((resolve, reject) => {
+    getAllFilmesFromRest()
+      .then((data) => {
+        resolve(data.length)
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+function createFilmeFromRest(filme) {
+  return new Promise((resolve, reject) => {
+    fetch(`${API_URL}/filmes`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(filme),
+    })
+      .then((response) => {
+        response
+          .json()
+          .then((data) => {
+            resolve(data)
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+function updateFilmeFromRest(id, filme) {
+  return new Promise((resolve, reject) => {
+    fetch(`${API_URL}/filmes/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(filme),
+    })
+      .then((response) => {
+        response
+          .json()
+          .then((data) => {
+            resolve(data)
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+function deleteFilmeFromRest(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`${API_URL}/filmes/${id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        resolve()
+      })
+      .catch((error) => {
+        reject(error)
+      })
+  })
+}
+
+export { 
+  getAllFilmesFromRest, 
+  getFilmesCountFromRest, 
+  createFilmeFromRest, 
+  updateFilmeFromRest, 
+  deleteFilmeFromRest,
+ }
