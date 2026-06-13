05ae56df9e83592543f884a094e1e54a42cd4b89
gfreitas15 <gfmonguilhott793@gmail.com>
2026-06-13 15:01:12 -0300

    refactor

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -6,10 +6,11 @@ const routes = [
       { path: '', component: () => import('@/pages/HomePage.vue') },
       { path: 'filmes', component: () => import('@/pages/FilmesPage.vue') },
       { path: 'cinemas', component: () => import('@/pages/CinemasPage.vue') },
-      { path: 'generos', component: () => import('@/pages/GenerosPage.vue') },
+     /* { path: 'generos', component: () => import('@/pages/GenerosPage.vue') },
       { path: 'clientes', component: () => import('@/pages/ClientesPage.vue') },
       { path: 'funcionarios', component: () => import('@/pages/FuncionariosPage.vue') },
       { path: 'pagamentos', component: () => import('@/pages/PagamentosPage.vue') },
+       */
     ],
   },
 
